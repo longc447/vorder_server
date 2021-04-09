96814cb777f07f0aaddb6d7f97eae3c08e626f81
344403155@qq.com <344403155@qq.com>
2021-04-09 13:45:51 +0800

    查询同步返回结果，优化menuModel类

--- model/menu.js
+++ model/menu.js
@@ -1,33 +1,31 @@
-// class menu{
-//     constructor(){
-
-//     }
-// }
-// module.exports=new menu();
-
 const menuCollection = require("../config/MongoDBConnection").getCollection("menu")
-
-exports.save = async (menu) => {
-    console.log(menu)
-    try {
-        const col = await menuCollection();
-        const result = await col.insertOne(menu);
-        return result.pos && result.ops[0];
-    } catch (error) {
-        throw "添加文章到数据库出错"
+class menuModel {
+    constructor() {
+        this.result=null;
+    }
+    async insert(menu) {
+        console.log(menu)
+        try {
+            const col = await menuCollection();
+            const result = await col.insertOne(menu);
+            return result.pos && result.ops[0];
+        } catch (error) {
+            throw "添加文章到数据库出错"
+        }
+    }
+    async query(menu) {
+        try {
+            const col = await menuCollection();
+            return await col.find(menu).toArray()
+            // .toArray((err, result) => {
+            //     if (err) throw err;
+            //     this.result=result;
+            //     console.log(result,this.result,this)
+            // });
+            // console.log(result,this.result)
+        } catch (error) {
+            throw "查询这里报错了"
+        }
     }
 }
-
-exports.find = async (menu) => {
-    try {
-        const col = await menuCollection();
-        const result = await col.find(menu).toArray((err, result)=>{
-            if (err) throw err;
-            console.log(result);
-        });
-        // console.log(result)
-        return result;
-    } catch (error) {
-
-    }
-}
+module.exports=new menuModel();
--- routes/menu.js
+++ routes/menu.js
@@ -5,10 +5,9 @@ route.get("/", async (req, res) => {
     // res.send("/menu get")
     console.log("进来了")
     try {
-        const menus=await menuModel.find({});
-        // console.log(menus)
+        const menus=await menuModel.query({})
         // res.send()
-        res.status(200).json({"status":"200"})
+        res.status(200).json(menus)
     } catch (error) {
         console.error(error);
         res.status(500).send();
@@ -19,10 +18,10 @@ route.post("/", async (req, res) => {
     // res.send("/menu post")
     console.log(req.body)
     try {
-        const newMenu = await menuModel.save({
-            "name":"四喜丸子",
-            "pic":""
-        })
+        // let newMenu = await menuModel.insert({
+        //     "name":"四喜丸子",
+        //     "pic":""
+        // })
         res.status(201).json(newMenu)
     } catch (error) {
         console.error(error);
